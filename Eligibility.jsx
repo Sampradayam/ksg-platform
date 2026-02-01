224d843951cf856a883692af1048a692e6e94eaf
Bhavya <s221081@rguktsklm.ac.in>
2026-02-01 12:44:20 +0530

    Add files via upload

---
+++ Eligibility.jsx
@@ -0,0 +1,10 @@
+export default function Eligibility() {
+    return (
+        <section>
+            <h2>Eligibility</h2>
+            <ul className="eligibility-list">
+                <li>Open to all age groups</li>
+            </ul>
+        </section>
+    );
+}
